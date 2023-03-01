5d9294f3d31cf5104650d5bcdabb922d2fab449b
Bsluykaat <kerumit2002@gmail.com>
2023-03-01 16:22:32 +0200

    Update TestTaskApi.kt

--- core/src/main/java/com/kerumitbsl/core/components/rest/TestTaskApi.kt
+++ core/src/main/java/com/kerumitbsl/core/components/rest/TestTaskApi.kt
@@ -1,14 +1,8 @@
 package com.kerumitbsl.core.components.rest
 
-import com.google.gson.JsonElement
 import com.kerumitbsl.core.bean.response.GetGifsResponse
 import com.kerumitbsl.core.extensions.GIPHY_INIT_KEY
-import okhttp3.MultipartBody
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface TestTaskApi {
